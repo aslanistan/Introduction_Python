92967ab7f5ae849b88f6bde4677c6ba250044b1d
aslanistan <aslan@txstate.edu>
2023-11-13 05:08:19 -0600

    Add files via upload

---
+++ IEEE_Python_Introduction/IEEE_Python_Introduction_L1/add_num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+int main() {
+   int a = 3;
+   int b = 4;
+   int sum;
+ 
+   sum = a + b;
+ 
+   printf("Sum : %d", sum);
+ 
+   return(0);
+}
---
+++ IEEE_Python_Introduction/IEEE_Python_Introduction_L1/add_num_f.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+int main() {
+   float a = 3;
+   float b = 4;
+   float sum;
+ 
+   sum = a + b;
+ 
+   printf("Sum : %d", sum);
+ 
+   return(0);
+}
